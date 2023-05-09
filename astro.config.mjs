393d997d04c04d2965326295f01929df47cf7714
Peter Holmes <ppholmes@usc.edu>
2023-05-09 16:21:32 -0700

    updated env vb location

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import sanity from "astro-sanity";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import {PROJECT_ID} from "./src/env"
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
-    projectId: PROJECT_ID,
+    projectId: context.env.PROJECT_ID,
     dataset: 'production',
     apiVersion: '2023-05-01'
     }), svelte(), tailwind()]
